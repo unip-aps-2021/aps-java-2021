5ccbe432f0560a8df3f644b0fd43c077f3f018e3
Flavio Freitas Pinter <f@pinter.dev>
2021-04-04 18:08:56 -0300

    UI format

--- src/main/java/TesteController.java
+++ src/main/java/TesteController.java
@@ -1,2 +1,14 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.TextAlignment;
+
 public class TesteController {
+
+    @FXML
+    private Label a;
+
+    @FXML
+    public void initialize(){
+        a.setTextAlignment(TextAlignment.JUSTIFY);
+    }
 }
