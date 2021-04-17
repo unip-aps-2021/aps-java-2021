1e2ac6593827dd819336fc16c57844d1278eac55
Flavio Freitas Pinter <f@pinter.dev>
2021-04-17 00:51:49 -0300

    Abas 1 e 2 completas

--- src/main/java/Pagina2Controller.java
+++ src/main/java/Pagina2Controller.java
@@ -16,7 +16,20 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Pagina2Controller {
     private final XYChart.Series series1 = new XYChart.Series();
     private final XYChart.Series series2 = new XYChart.Series();
-
+    private final XYChart.Series series3 = new XYChart.Series();
+    private final XYChart.Series series4 = new XYChart.Series();
+    private Rio tiete = new Rio("Tietê", 1000, 500, "Horrível");
+    private Rio pinheiros = new Rio("Pinheiros", 4000, 2800, "Pior que a anterior");
+    private Rio tamanduatei = new Rio("Tamanduatei", 5000, 3600, "Ruim pra krl");
+    private Rio piracicaba = new Rio("Piracicaba", 3000, 2300, "Horrível");
+    private Rio anhagabau = new Rio("Anhagabau", 7000, 4000, "Um lixo");
+    private Rio paraibadosul = new Rio("Guarapiranga", 4500, 3600, "Uma bosta");
+    private Rio aricanduva = new Rio("Aricanduva", 28, 20, "Horrível");
+    private Rio kurupira = new Rio("Kurupira", 45, 35, "Pior que a anterior");
+    private Rio jacare = new Rio("Jacare", 60, 35, "Ruim pra krl");
+    private Rio peixe = new Rio("Peixe", 90, 45, "Horrível");
+    private Rio gato = new Rio("Gato", 50, 37, "Um lixo");
+    private Rio myu = new Rio("Myu", 120, 95, "Fedido");
 
     @FXML
     private TabPane tabPanePag2;
@@ -54,29 +67,23 @@ public class Pagina2Controller {
         new CategoryAxis().setLabel("Mike");
         new NumberAxis().setLabel("Ueee");
         series1.setName("Unip");
-        Rio tiete = new Rio("Tietê", 1000, 500, "Horrível");
-        Rio pinheiros = new Rio("Pinheiros", 4000, 2800, "Pior que a anterior");
-        Rio tamanduatei = new Rio("Tamanduatei", 5000, 3600, "Ruim pra krl");
-        Rio piracicaba = new Rio("Piracicaba", 3000, 2300, "Horrível");
-        Rio anhagabau = new Rio("Anhagabau", 7000, 4000, "Um lixo");
-        Rio paraibadosul = new Rio("Guarapiranga", 4500, 3600, "Uma bosta");
         tiete.setPrimeiraCuriosidade("O Rio Tietê possuí 1.150km de extensão.");
-        tiete.setSegundaCuriosidade("1.100km de sua extensão é poluída e tóxica para peixes.");
+        tiete.setSegundaCuriosidade("1.100km de sua extensão é poluída e tóxica para os seres vivos.");
         tiete.setTerceiraCuriosidade("Em Mogi das Cruzes ele recebe cerca de 60 toneladas de esgoto por dia.");
         pinheiros.setPrimeiraCuriosidade("O Rio Pinheiros possuí 25km de extensão.");
-        pinheiros.setSegundaCuriosidade("20km de sua extensão é poluída e tóxica para peixes.");
+        pinheiros.setSegundaCuriosidade("20km de sua extensão é poluída e tóxica para os seres vivos.");
         pinheiros.setTerceiraCuriosidade("O Rio Pinheiros nasce do encontro do Rio Guarapiranga com o Rio Grande.");
         tamanduatei.setPrimeiraCuriosidade("O Rio Tamanduatei possuí 35km de extensão.");
-        tamanduatei.setSegundaCuriosidade("28km de sua extensão é poluída e tóxica para peixes.");
+        tamanduatei.setSegundaCuriosidade("28km de sua extensão é poluída e tóxica para os seres vivos.");
         tamanduatei.setTerceiraCuriosidade("Antigamente o rio era usado para lavagem de roupas, banho e outras atividades.");
         piracicaba.setPrimeiraCuriosidade("O Rio Piracicaba possuí 115km de extensão.");
-        piracicaba.setSegundaCuriosidade("105km de sua extensão é poluída e tóxica para peixes.");
+        piracicaba.setSegundaCuriosidade("105km de sua extensão é poluída e tóxica para os seres vivos.");
         piracicaba.setTerceiraCuriosidade("Suas Quedas d'água bloqueiam a migração dos peixes.");
         anhagabau.setPrimeiraCuriosidade("O Rio Anhagabaú possuí 137km de extensão.");
-        anhagabau.setSegundaCuriosidade("127km de sua extensão é poluída e tóxica para peixes.");
+        anhagabau.setSegundaCuriosidade("127km de sua extensão é poluída e tóxica para os seres vivos.");
         anhagabau.setTerceiraCuriosidade("A atual Praça da Bandeira, era, tradicionalmente, um grande charco.");
         paraibadosul.setPrimeiraCuriosidade("O Rio Paraíba do Sul possuí 1.137km de extensão.");
-        paraibadosul.setSegundaCuriosidade("1.100km de extensão é poluída e tóxica para peixes.");
+        paraibadosul.setSegundaCuriosidade("1.100km de extensão é poluída e tóxica para os seres vivos.");
         paraibadosul.setTerceiraCuriosidade("É o rio mais importante do estado do Rio de Janeiro");
         addChart(series1, "Tio ete", (int) tiete.getPorcentagemPoluicao());
         addChart(series1, "Pinheiros", (int) pinheiros.getPorcentagemPoluicao());
@@ -94,6 +101,8 @@ public class Pagina2Controller {
         try {
             if (chart.getData() != null) {
                 chart.getData().removeAll(series2);
+                chart.getData().removeAll(series3);
+                chart.getData().removeAll(series4);
                 chart.getData().addAll(series1);
             }
         } catch (IllegalArgumentException | NullPointerException e) {
@@ -103,21 +112,61 @@ public class Pagina2Controller {
 
     @FXML
     private void setAba2(ActionEvent event) {
-        Rio aricanduva = new Rio("Aricanduva", 1000, 500, "Horrível");
-        Rio kurupira = new Rio("Kurupira", 4000, 2800, "Pior que a anterior");
-        Rio jacare = new Rio("Jacare", 5000, 3600, "Ruim pra krl");
-        Rio peixe = new Rio("Peixe", 3000, 2300, "Horrível");
-        Rio gato = new Rio("Gato", 7000, 4000, "Um lixo");
+        aricanduva.setPrimeiraCuriosidade("O Rio Aricanduva tem 28km de extensão.");
+        aricanduva.setSegundaCuriosidade("23km da sua extensão é poluida e toxica para os seres vivos.");
+        aricanduva.setTerceiraCuriosidade("Esse rio nasce poluído, pois uma de suas nascentes localiza-se no aterro sanitario Sao Joao");
+        kurupira.setPrimeiraCuriosidade("O Rio Kurupira tem 45km de extensão.");
+        kurupira.setSegundaCuriosidade("40km da sua extensão é poluida e toxica para os seres vivos.");
+        kurupira.setTerceiraCuriosidade("A agua corre para um sentido diferente.");
+        jacare.setPrimeiraCuriosidade("O Rio Jacare tem 60km de extensão.");
+        jacare.setSegundaCuriosidade("56km da sua extensão é poluida e toxica para os seres vivos.");
+        jacare.setTerceiraCuriosidade("No rio Jacare, os jacares que vivem lá adoecem com o lixo.");
+        peixe.setPrimeiraCuriosidade("O Rio Peixe tem 90km de extensão.");
+        peixe.setSegundaCuriosidade("Metade da sua extensão é poluida e toxica para os seres vivos.");
+        peixe.setTerceiraCuriosidade("É o rio de São Paulo com maior variedade na fauna marinha");
+        gato.setPrimeiraCuriosidade("O Rio Gato tem 50km de extensão.");
+        gato.setSegundaCuriosidade("45km de sua extensão é poluida e toxica para os seres vivos.");
+        gato.setTerceiraCuriosidade("Esse rio é mais afetado pelas chuvas, que levam mais poluição às suas águas.");
+        myu.setPrimeiraCuriosidade("O Rio Myu tem 120km de extensão.");
+        myu.setSegundaCuriosidade("95km da sua extensão é poluida e toxica para os seres vivos");
+        myu.setTerceiraCuriosidade("O Rio myu é nomeado por conta dos imigrantes asiáticos que viviam nas suas redondezas");
         addChart(series2, aricanduva.getNome(), (int) aricanduva.getPorcentagemPoluicao());
         addChart(series2, kurupira.getNome(), (int) kurupira.getPorcentagemPoluicao());
         addChart(series2, jacare.getNome(), (int) jacare.getPorcentagemPoluicao());
         addChart(series2, peixe.getNome(), (int) peixe.getPorcentagemPoluicao());
         addChart(series2, gato.getNome(), (int) gato.getPorcentagemPoluicao());
-
+        addChart(series2, myu.getNome(), (int) myu.getPorcentagemPoluicao());
+        rotate(aricanduva, kurupira, jacare, peixe, gato, myu);
 
         try {
             chart.getData().removeAll(series1);
+            chart.getData().removeAll(series3);
+            chart.getData().removeAll(series4);;
             chart.getData().addAll(series2);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            e.addSuppressed(e);
+        }
+    }
+
+    @FXML
+    private void setAba3(ActionEvent event){
+        try {
+            chart.getData().removeAll(series1);
+            chart.getData().removeAll(series2);
+            chart.getData().removeAll(series4);;
+            chart.getData().addAll(series3);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            e.addSuppressed(e);
+        }
+    }
+
+    @FXML
+    private void setAba4(ActionEvent event){
+        try {
+            chart.getData().removeAll(series1);
+            chart.getData().removeAll(series3);
+            chart.getData().removeAll(series2);;
+            chart.getData().addAll(series4);
         } catch (IllegalArgumentException | NullPointerException e) {
             e.addSuppressed(e);
         }
@@ -144,8 +193,8 @@ public class Pagina2Controller {
                             Rio quarto,
                             Rio quinto,
                             Rio sexto) {
-        int delay = 10000;
-        int interval = 5000;
+        int delay = 1000;
+        int interval = 18000;
         Timer timer = new Timer();
         List<String> listaTextos = new ArrayList<>(10);
         listaTextos.add(primeiro.getPrimeiraCuriosidade() + "\n" + primeiro.getSegundaCuriosidade() + "\n" + primeiro.getTerceiraCuriosidade());
@@ -167,6 +216,11 @@ public class Pagina2Controller {
                 textoCabeludo.setText(listaTextos.get(num3));
             }
         }, delay, interval);
-
     }
 }
+//
+//
+//
+//
+//
+//
