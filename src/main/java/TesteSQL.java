20b72077decfabac6f3ebed7b802a1d8e7f18960
Flavio Freitas Pinter <f@pinter.dev>
2021-04-24 00:39:34 -0300

    Banco de dados azure implementado

---
+++ src/main/java/TesteSQL.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class TesteSQL {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String dbUrl = "jdbc:sqlserver://aps2.database.windows.net:1433;database=aps;user=adminaps@aps2;password=Kootra300;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+        Connection conn = DriverManager.getConnection(dbUrl);
+        if (conn != null) {
+            System.out.println("CONEKITADU");
+        }
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM TESTE;");
+        while (rs.next()) {
+            System.out.println(rs.getString("RIO"));
+        }
+
+    }
+}
