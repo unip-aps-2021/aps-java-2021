3e2a8716913069e0c41bcb21ae6bbdeb719aa074
Flávio Freitas Pinter <48632598+flaviopinter@users.noreply.github.com>
2021-04-24 19:28:34 -0300

    DAO criado.

---
+++ src/main/java/DAO.java
@@ -0,0 +1,58 @@
+import java.sql.*;
+import java.util.HashMap;
+
+public class DAO {
+    Connection conn;
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        System.out.println(DAO.getPrimeiraCuriosidade("Tietê"));
+        System.out.println(DAO.getSegundaCuriosidade("Tietê"));
+        System.out.println(DAO.getTerceiraCuriosidade("Tietê"));
+    }
+
+    public DAO() throws ClassNotFoundException, SQLException {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String dbUrl = "jdbc:sqlserver://aps2.database.windows.net:1433;database=aps;user=adminaps@aps2;password=Kootra300;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+        conn = DriverManager.getConnection(dbUrl);
+    }
+
+    public static String getPrimeiraCuriosidade(String nomeRio) {
+        String curiosidade = "";
+        try {
+            curiosidade = new DAO().getCuriosidade("PrimeiraCuriosidade").get(nomeRio);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return curiosidade;
+    }
+
+    public static String getSegundaCuriosidade(String nomeRio) {
+        String curiosidade = "";
+        try {
+            curiosidade = new DAO().getCuriosidade("SegundaCuriosidade").get(nomeRio);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return curiosidade;
+    }
+
+    public static String getTerceiraCuriosidade(String nomeRio) {
+        String curiosidade = "";
+        try {
+            curiosidade = new DAO().getCuriosidade("TerceiraCuriosidade").get(nomeRio);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return curiosidade;
+    }
+
+    private HashMap<String, String> getCuriosidade(String curiosidade) throws SQLException {
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery(String.format("SELECT Nome, %s FROM RIOS", curiosidade));
+        HashMap<String, String> hashCuriosidade = new HashMap<>();
+        while (rs.next()) {
+            hashCuriosidade.put(rs.getString("Nome"), rs.getString(curiosidade));
+        }
+        return hashCuriosidade;
+    }
+}
