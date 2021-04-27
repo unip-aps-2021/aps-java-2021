620541194d17a4286e5cd0957558f3c73b387c35
Flavio Freitas Pinter <f@pinter.dev>
2021-04-26 23:53:33 -0300

    Pequenas mudanças na classe DAO

--- src/main/java/DAO.java
+++ src/main/java/DAO.java
@@ -1,5 +1,7 @@
 import java.sql.*;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 public class DAO {
     Connection conn;
@@ -9,6 +11,7 @@ public class DAO {
         String dbUrl = "jdbc:sqlserver://aps2.database.windows.net:1433;database=aps;user=adminaps@aps2;password=Kootra300;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
         conn = DriverManager.getConnection(dbUrl);
     }
+
 
     public static String getPrimeiraCuriosidade(String nomeRio) {
         String curiosidade = "";
@@ -42,11 +45,21 @@ public class DAO {
 
     private HashMap<String, String> getCuriosidade(String curiosidade) throws SQLException {
         Statement statement = conn.createStatement();
-        ResultSet rs = statement.executeQuery(String.format("SELECT Nome, %s FROM RIOS", curiosidade));
+        ResultSet rs = statement.executeQuery(String.format("SELECT Nome, %s FROM RIOS;", curiosidade));
         HashMap<String, String> hashCuriosidade = new HashMap<>();
         while (rs.next()) {
             hashCuriosidade.put(rs.getString("Nome"), rs.getString(curiosidade));
         }
         return hashCuriosidade;
+    }
+
+    public HashMap<String, Map<String, String>> getTextos() throws SQLException {
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM CONSEQUENCIAS;");
+        HashMap<String, Map<String, String>> textosHash = new HashMap<>();
+        while (rs.next()){
+            textosHash.put(rs.getString("Id"), Collections.singletonMap(rs.getString("Titulo"), rs.getString("Texto")));
+        }
+        return textosHash;
     }
 }
